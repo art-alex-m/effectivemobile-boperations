e45cfb4ec094c51a6357aa9c3e7370c3bac35e7f
Alexander M Artamonov <art.alex.m@gmail.com>
2024-02-29 15:45:07 +0300

    Инициация структуры проекта

---
+++ webapp/src/main/java/ru/effectivemobile/boperations/BoperationsApplication.java
@@ -0,0 +1,11 @@
+package ru.effectivemobile.boperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoperationsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BoperationsApplication.class, args);
+    }
+}
---
+++ webapp/src/test/java/ru/effectivemobile/boperations/TestBoperationsApplication.java
@@ -0,0 +1,23 @@
+package ru.effectivemobile.boperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestBoperationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(BoperationsApplication::main).with(TestBoperationsApplication.class).run(args);
+    }
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+    }
+
+}
